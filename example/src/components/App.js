2c8095c67be32077be4b1fffb7e925e80f4c07b5
martinandert <mandert@gmail.com>
2015-04-17 14:36:45 +0200

    example: add repo link [ci skip]

--- example/src/components/App.js
+++ example/src/components/App.js
@@ -3,14 +3,16 @@ import StyleSheet from 'react-inline';
 import MediaQueries from './MediaQueries';
 import ButtonList from './ButtonList';
 
-const url = 'https://github.com/martinandert/react-inline/tree/master/example/src/components';
+const repoUrl = 'https://github.com/martinandert/react-inline';
+const codeUrl = `${repoUrl}/tree/master/example/src/components`;
 
 class App extends React.Component {
   render() {
     return (
       <div className={styles.root}>
+        <p>This is a small demonstration of what <a href={repoUrl}>React Inline</a> is capable of.</p>
         <p>Use your browser's "Inspect Element" tool to see inline styles turned into class names.</p>
-        <p>You can find the code for all components <a href={url}>here</a>.</p>
+        <p>You can find the code for all components <a href={codeUrl}>here</a>.</p>
 
         <MediaQueries />
         <ButtonList />
